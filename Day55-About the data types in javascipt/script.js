5d5d5f6f8d90e8b2254755c5d6b3644776555bb8
Gagan3512 <gagan1825.cse18@chitkara.edu.in>
2023-11-21 15:10:42 -0500

    data types added

---
+++ Day55-About the data types in javascipt/script.js
@@ -0,0 +1,31 @@
+console.log("Hey this is tutorial 55 of JS");
+
+var a = 5;
+var b = 6;
+var c = "Harry";
+//var 55a = "Rohan";// not allowed 
+
+
+console.log(a+b+8);
+console.log(typeof a, typeof b, typeof c);
+
+{
+    a = 11;
+    console.log("A is " + a );
+}
+
+const a1 = 6;
+// a1 = a1 + 1; // not allowerd as it a1 is constant 
+
+console.log(a1);
+
+
+let o = {
+    name: "Gagan",
+    "job code": 5600,
+
+}
+
+console.log(o);
+o.salary = "100crores";
+console.log(o);
