6cf6deab53098fd9791690e4f5b3d1fc69942f28
Gagan3512 <gagan1825.cse18@chitkara.edu.in>
2023-11-21 21:17:15 -0500

    loops

---
+++ Day56-About if else/index.js
@@ -0,0 +1,19 @@
+console.log("Hello i am running the code");
+
+let age = 45;
+let grace = 2;
+
+age += grace;
+console.log(age);
+console.log(age + grace);
+console.log(age - grace);
+console.log(age * grace);
+console.log(age / grace);
+console.log(age ** grace);
+
+if(age > 18){
+    console.log("You can drive");
+}
+else{
+    console.log("You cannot drive");
+}
