edb2f146d0981d429f6f744dbb98bc856db5fa9c
gagan3512 <gagan1825.cse18@chitkara.edu.in>
2024-02-16 13:26:33 -0500

    react code updated

---
+++ Day113/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+
+      <nav>
+        <ul>
+        <li>Home</li>
+        <li>About </li>
+        <li>Contact us</li>
+
+        </ul>
+        
+      </nav>
+
+  )
+}
+
+export default Navbar
